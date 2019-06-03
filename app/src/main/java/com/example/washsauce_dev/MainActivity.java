e557366aca98ec483ca2f722210a09cbfe8ac892
Lucas Wargha <war18001@byui.edu>
2019-06-03 11:46:04 -0600

    Database add randomly

--- app/src/main/java/com/example/washsauce_dev/MainActivity.java
+++ app/src/main/java/com/example/washsauce_dev/MainActivity.java
@@ -3,16 +3,21 @@ package com.example.washsauce_dev;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static android.widget.Toast.*;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -32,24 +37,25 @@ public class MainActivity extends AppCompatActivity {
         editLocation = findViewById(R.id.location);
     }
     public void saveTask(View v) {
-        Toast.makeText(MainActivity.this, "Starting to save", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "Starting to save", LENGTH_SHORT).show();
         String name = editName.getText().toString();
         String location = editLocation.getText().toString();
         Map<String, Object> task = new HashMap<>();
         task.put(KEY_NAME, name);
         task.put(KEY_LOCATION, location);
-
-        db.collection("users").document("First Doc").set(task)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//       Toast.makeText(MainActivity.this, name + " " + location, LENGTH_SHORT).show();
+        db.collection("users")
+                .add(task)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(MainActivity.this, "Task Saved", Toast.LENGTH_SHORT).show();
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(MainActivity.this, "Document Added", LENGTH_SHORT).show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        Log.w(TAG, "Error adding document", e);
                     }
                 });
     }

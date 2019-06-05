f2beb86cc2a62ca4bd8b3e7f9c4ae6ef664bde6c
Lucas Wargha <war18001@byui.edu>
2019-06-04 20:45:40 -0600

    reader and writer classes working

--- app/src/main/java/com/example/washsauce_dev/DataBaseWriter.java
+++ app/src/main/java/com/example/washsauce_dev/DataBaseWriter.java
@@ -1,4 +1,38 @@
 package com.example.washsauce_dev;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+import java.util.Map;
+
+
 public class DataBaseWriter {
+    private static final String TAG = "MainActivity";
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void addNewUser(Map<String, Object> data) {
+            db.collection("users")
+                    .add(data)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.w(TAG, "Document added with success!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error adding document", e);
+                        }
+                    });
+    }
+
+    String type;
 }
--- app/src/main/java/com/example/washsauce_dev/MainActivity.java
+++ app/src/main/java/com/example/washsauce_dev/MainActivity.java
@@ -1,18 +1,9 @@
 package com.example.washsauce_dev;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,38 +16,29 @@ public class MainActivity extends AppCompatActivity {
     private static final String KEY_LOCATION = "location";
 
     private EditText editName;
-    private EditText editLocation;
+    private EditText editPw;
 
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editName = findViewById(R.id.userName);
-        editLocation = findViewById(R.id.location);
+        editPw = findViewById(R.id.password);
     }
     public void saveTask(View v) {
-        Toast.makeText(MainActivity.this, "Starting to save", LENGTH_SHORT).show();
         String name = editName.getText().toString();
-        String location = editLocation.getText().toString();
-        Map<String, Object> task = new HashMap<>();
-        task.put(KEY_NAME, name);
-        task.put(KEY_LOCATION, location);
-//       Toast.makeText(MainActivity.this, name + " " + location, LENGTH_SHORT).show();
-        db.collection("users")
-                .add(task)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(MainActivity.this, "Document Added", LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
+        String location = editPw.getText().toString();
+        Map<String, Object> user = new HashMap<>();
+        user.put(KEY_NAME, name);
+        user.put(KEY_LOCATION, location);
+        DataBaseWriter writeUser = new DataBaseWriter();
+        writeUser.addNewUser(user);
+        makeText(this, "Added Successfully!", LENGTH_SHORT).show();
+        DataBaseReader d = new DataBaseReader();
+        d.readUser("EdjgkfBAKLZvVHPZY2bp");
+    }
+
+    public void startLoginActivity(View view) {
     }
 }

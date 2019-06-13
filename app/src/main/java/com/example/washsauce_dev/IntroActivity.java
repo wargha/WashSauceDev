b792d0bc4d26ab9e7bb1f2cf86eeb563f33ea632
Lucas Wargha <war18001@byui.edu>
2019-06-12 19:43:51 -0600

    More Activities

--- app/src/main/java/com/example/washsauce_dev/IntroActivity.java
+++ app/src/main/java/com/example/washsauce_dev/IntroActivity.java
@@ -17,32 +17,34 @@ public class IntroActivity extends AppCompatActivity {
     private static final String KEY_PASSWORD = "password";
     private EditText editName;
     private EditText editPw;
-    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.activity_login);
 
         editName = findViewById(R.id.userName);
         editPw = findViewById(R.id.password);
     }
     public void authenticateUser(View v) {
-        String name = editName.getText().toString();
-        String pw = editPw.getText().toString();
-        Map<String, Object> user = new HashMap<>();
-        user.put(KEY_NAME, name);
-        user.put(KEY_PASSWORD, pw);
-        DataBaseWriter writeUser = new DataBaseWriter(this);
-        writeUser.addNewUser(user);
-        makeText(this, "Added Successfully!", LENGTH_SHORT).show();
-        DataBaseReader d = new DataBaseReader(this);
-        d.readUser("EdjgkfBAKLZvVHPZY2bp");
+//        String name = editName.getText().toString();
+//        String pw = editPw.getText().toString();
+//        Map<String, Object> user = new HashMap<>();
+//        user.put(KEY_NAME, name);
+//        user.put(KEY_PASSWORD, pw);
+//        DataBaseWriter writeUser = new DataBaseWriter(this);
+//        writeUser.addNewUser(user);
+//        makeText(this, "Added Successfully!", LENGTH_SHORT).show();
+//        DataBaseReader d = new DataBaseReader(this);
+//        d.readUser("EdjgkfBAKLZvVHPZY2bp");
+        Intent i = new Intent(this, UserHomeActivity.class);
+        startActivity(i);
     }
 
     public void startSignUpActivity (View view) {
         Intent i = new Intent(this, SignUpActivity.class);
         startActivity(i);
     }
+
 
 }

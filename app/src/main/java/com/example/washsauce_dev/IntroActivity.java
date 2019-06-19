07e1e53a3f78d5380766bcb791a45c15e96c1989
Lucas Wargha <war18001@byui.edu>
2019-06-19 14:21:22 -0600

    New shared preferences changes

--- app/src/main/java/com/example/washsauce_dev/IntroActivity.java
+++ app/src/main/java/com/example/washsauce_dev/IntroActivity.java
@@ -1,6 +1,8 @@
 package com.example.washsauce_dev;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,15 +19,30 @@ public class IntroActivity extends AppCompatActivity {
     private static final String KEY_PASSWORD = "password";
     private EditText editName;
     private EditText editPw;
-
+   private SharedPreferences sharedPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         editName = findViewById(R.id.userName);
         editPw = findViewById(R.id.password);
+        sharedPref = getSharedPreferences("authPref", Context.MODE_PRIVATE);
+        Boolean isAuthenticated = sharedPref.getBoolean("authRef", true);
+        if (isAuthenticated) {
+            Intent i = new Intent(this, UserHomeActivity.class);
+            startActivity(i);
+        }
     }
+
+    protected void onStop() {
+        // call the superclass method first
+        super.onStop();
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean("authRef", true);
+        editor.commit();
+    }
+
     public void authenticateUser(View v) {
         String email = editName.getText().toString();
         String password = editPw.getText().toString();
@@ -46,6 +63,4 @@ public class IntroActivity extends AppCompatActivity {
         Intent i = new Intent(this, SignUpActivity.class);
         startActivity(i);
     }
-
-
 }

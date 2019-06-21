87014d13f5c47f544e8ee3dc373625782a2dc7b5
nmv13 <vel16002@byui.edu>
2019-06-20 20:46:04 -0600

    request design

--- app/src/main/java/com/example/washsauce_dev/WasherHomeActivity.java
+++ app/src/main/java/com/example/washsauce_dev/WasherHomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.washsauce_dev;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WasherHomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class WasherHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_washer_home);
+    }
+
+    public void customerRequests(View view) {
+        Intent i = new Intent(this, CustomerRequestActivity.class);
+        startActivity(i);
     }
 }

04be019ced384bd29ca67bdc3878d81ae710e163
Lucas Wargha <war18001@byui.edu>
2019-07-03 10:02:17 -0600

    Task obj

--- app/src/main/java/com/example/washsauce_dev/RequestTaskActivity.java
+++ app/src/main/java/com/example/washsauce_dev/RequestTaskActivity.java
@@ -2,12 +2,31 @@ package com.example.washsauce_dev;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RequestTaskActivity extends AppCompatActivity {
-
+    private EditText email;
+    private EditText pw;
+    private EditText phone;
+    private EditText location;
+    private EditText name;
+    private Button buttonConfirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_task);
+        email = findViewById(R.id.email);
+        pw = findViewById(R.id.pw);
+        phone = findViewById(R.id.phone);
+        location = findViewById(R.id.location);
+        name = findViewById(R.id.name);
+    }
+
+    private void clickButton() {
+        String emailDB = email.getText().toString();
+//        Task t = new Task();
     }
 }
--- app/src/main/java/com/example/washsauce_dev/Task.java
+++ app/src/main/java/com/example/washsauce_dev/Task.java
@@ -1,10 +1,117 @@
 package com.example.washsauce_dev;
 
 public class Task {
-    private Date requestDate;
-    private Date acceptDate;
-    private String comments;
-    private Status status;
-    private Boolean isFinished;
+    protected Date requestDate;
+    protected Date acceptDate;
+    protected Date finishDate;
+    protected String comments;
+    protected String status;
+    protected Boolean isFinished;
+    protected String requestorEmail;
+    protected int numberOfLoads;
+    protected String condition;
+    protected String loadType;
+    protected float price;
 
+    public Task(Date requestDate, Date acceptDate, Date finishDate, String comments, String status, Boolean isFinished, String requestorEmail, int numberOfLoads, String condition, String loadType, float price) {
+        this.requestDate = requestDate;
+        this.acceptDate = acceptDate;
+        this.finishDate = finishDate;
+        this.comments = comments;
+        this.status = status;
+        this.isFinished = isFinished;
+        this.requestorEmail = requestorEmail;
+        this.numberOfLoads = numberOfLoads;
+        this.condition = condition;
+        this.loadType = loadType;
+        this.price = price;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public Date getAcceptDate() {
+        return acceptDate;
+    }
+
+    public void setAcceptDate(Date acceptDate) {
+        this.acceptDate = acceptDate;
+    }
+
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Boolean getFinished() {
+        return isFinished;
+    }
+
+    public void setFinished(Boolean finished) {
+        isFinished = finished;
+    }
+
+    public String getRequestorEmail() {
+        return requestorEmail;
+    }
+
+    public void setRequestorEmail(String requestorEmail) {
+        this.requestorEmail = requestorEmail;
+    }
+
+    public int getNumberOfLoads() {
+        return numberOfLoads;
+    }
+
+    public void setNumberOfLoads(int numberOfLoads) {
+        this.numberOfLoads = numberOfLoads;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getLoadType() {
+        return loadType;
+    }
+
+    public void setLoadType(String loadType) {
+        this.loadType = loadType;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }

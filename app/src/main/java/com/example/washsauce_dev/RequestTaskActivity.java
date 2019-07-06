b7589b558e7adab20bcb8d9b9df315d3e04fb334
nmv13 <vel16002@byui.edu>
2019-07-06 15:10:41 -0600

    customer task request

--- app/src/main/java/com/example/washsauce_dev/RequestTaskActivity.java
+++ app/src/main/java/com/example/washsauce_dev/RequestTaskActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,37 +17,34 @@ import com.google.firebase.auth.FirebaseAuth;
 public class RequestTaskActivity extends AppCompatActivity {
 
     private EditText loads;
-    private CheckBox sSmall;
-    private CheckBox sMedium;
-    private CheckBox sLarge;
-    private CheckBox cBasicDirty;
-    private CheckBox cMuddy;
-    private CheckBox cStained;
-    private CheckBox kDenim;
-    private CheckBox kWhites;
-    private CheckBox kLightColors;
-    private CheckBox kDarkColors;
-    private CheckBox kDelicate;
-    private CheckBox kBeddingTowel;
+    private RadioButton sSmall;
+    private RadioButton sMedium;
+    private RadioButton sLarge;
+    private RadioButton kCloths;
+    private RadioButton kBeddingTowel;
+    private RadioButton kOther;
+    private RadioButton cNormalDirty;
+    private RadioButton cMuddy;
+    private RadioButton cStained;
+    private EditText notes;
     private String userID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_task);
 
         loads         = findViewById(R.id.editText2);
-        sSmall        = findViewById(R.id.checkBox);
-        sMedium       = findViewById(R.id.checkBox2);
-        sLarge        = findViewById(R.id.checkBox4);
-        cBasicDirty   = findViewById(R.id.checkBox7);
-        cMuddy        = findViewById(R.id.checkBox8);
-        cStained      = findViewById(R.id.checkBox9);
-        kDenim        = findViewById(R.id.checkBox10);
-        kWhites       = findViewById(R.id.checkBox11);
-        kLightColors  = findViewById(R.id.checkBox12);
-        kDarkColors   = findViewById(R.id.checkBox13);
-        kDelicate     = findViewById(R.id.checkBox14);
-        kBeddingTowel = findViewById(R.id.checkBox15);
+        sSmall        = findViewById(R.id.radio_one);
+        sMedium       = findViewById(R.id.radio_two);
+        sLarge        = findViewById(R.id.radio_three);
+        kCloths       = findViewById(R.id.radio_four);
+        kBeddingTowel = findViewById(R.id.radio_five);
+        kOther        = findViewById(R.id.radio_six);
+        cNormalDirty  = findViewById(R.id.radio_seven);
+        cMuddy        = findViewById(R.id.radio_eight);
+        cStained      = findViewById(R.id.radio_nine);
+        notes         = findViewById(R.id.editText5);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         userID = preferences.getString("USER_ID_KEY", "");
     }
@@ -55,32 +53,37 @@ public class RequestTaskActivity extends AppCompatActivity {
 
        Integer iLoads = Integer.parseInt(loads.getText().toString());
 
-        Integer a,b,c,d,e,f,g,h,i,j,k,l;
+        Integer a,b,c,d,e,f,g,h,i;
 
         // Everything after the first ";" of each line is testing code to be deleted when
         // it is verified, same with the toasts below.
         Boolean iSmall        = sSmall.isChecked(); if(iSmall) {a = 1;}else {a = 0;}
         Boolean iMedium       = sMedium.isChecked(); if(iMedium) {b = 1;}else {b = 0;}
         Boolean iLarge        = sLarge.isChecked(); if(iLarge) {c = 1;}else {c = 0;}
-        Boolean iBasicDirty   = cBasicDirty.isChecked(); if(iBasicDirty) {d = 1;}else {d = 0;}
-        Boolean iMuddy        = cMuddy.isChecked(); if(iMuddy) {e = 1;}else {e = 0;}
-        Boolean iStained      = cStained.isChecked(); if(iStained) {f = 1;}else {f = 0;}
-        Boolean iDenim        = kDenim.isChecked(); if(iDenim) {g = 1;}else {g = 0;}
-        Boolean iWhites       = kWhites.isChecked(); if(iWhites) {h = 1;}else {h = 0;}
-        Boolean iLightColors  = kLightColors.isChecked(); if(iLightColors) {i = 1;}else {i = 0;}
-        Boolean iDarkColors   = kDarkColors.isChecked(); if(iDarkColors) {j = 1;}else {j = 0;}
-        Boolean iDelicate     = kDelicate.isChecked(); if(iDelicate) {k = 1;}else {k = 0;}
-        Boolean iBeddingTowel = kBeddingTowel.isChecked(); if(iBeddingTowel) {l = 1;}else {l = 0;}
+        Boolean iCloths       = kCloths.isChecked(); if(iCloths) {d = 1;}else {d = 0;}
+        Boolean iBeddingTowel = kBeddingTowel.isChecked(); if(iBeddingTowel) {e = 1;}else {e = 0;}
+        Boolean iOther        = kOther.isChecked(); if(iOther) {f = 1;}else {f = 0;}
+        Boolean iNormalDirty   = cNormalDirty.isChecked(); if(iNormalDirty) {g = 1;}else {g = 0;}
+        Boolean iMuddy        = cMuddy.isChecked(); if(iMuddy) {h = 1;}else {h = 0;}
+        Boolean iStained      = cStained.isChecked(); if(iStained) {i = 1;}else {i = 0;}
+
+        String iNotes         = notes.getText().toString();
 
         showToast(iLoads);
 
         showToast(a); showToast(b); showToast(c); showToast(d); showToast(e); showToast(f);
-        showToast(g); showToast(h); showToast(i); showToast(j); showToast(k); showToast(l);
+        showToast(g); showToast(h); showToast(i);
+
+        showToast2(iNotes);
 
         //Task t = new Task();
     }
 
     private void showToast(Integer text) {
         Toast.makeText(RequestTaskActivity.this, text.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void showToast2(String text) {
+        Toast.makeText(RequestTaskActivity.this, text, Toast.LENGTH_SHORT).show();
     }
 }
